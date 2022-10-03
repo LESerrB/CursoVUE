819bc31a27a2ac7911893f32b6568af431159a57
Luis Esteban Serrano <luis.serrano@biossmann.com>
2022-10-03 11:45:10 -0500

    Uso de metodo setup

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,27 @@
-<script setup>
-  const name = 'Vue dinámico';
-  const styleColor = "color: blue";
-  const arrayColors = ["blue", "red", "green"];
-  const activo = true;
-  const encendido = true;
+<script>
+  import{ref} from "vue";
+  
+  export default{
+    setup(){
+      const counter = ref(0);
+
+      const increment = () =>{
+        counter.value++
+      }
+
+      return{
+        counter,
+        increment,
+      }
+    }
+  };
 </script>
 
 <template>
-  <H1>Hola {{name}}</H1>
-
-  <br>
-  <h2 :style="styleColor">Azul</h2>
-
-  <br>
-  <h2 :style="`color: ${arrayColors[2]}`">No azul</h2>
-
-  <h2>
-    {{activo ? "Activo" : "Inactivo"}}
-  </h2>
-
-  <br>
-  <h2 v-if="encendido === true">
-    <span>Encendido</span>
-    fadrg
-  </h2>
-  <h2 v-else-if="encendido === false">Apagado</h2>
-  <h2 v-else>Indeciso</h2>
-
-  <br>
-  <h2 v-show="activo">Visible</h2>
+  <h1>APP</h1>
+  <button @click="increment">{{counter}}</button>
 </template>
 
 <style>
-  h1{
-    color: red;
-  }
+  
 </style>
