dd1ddc27f2c0f919736847a2930adc53a37e7178
Luis Esteban Serrano <luis.serrano@biossmann.com>
2022-10-12 14:46:21 -0500

    Autenticación de email

--- firebase-app/src/App.vue
+++ firebase-app/src/App.vue
@@ -9,8 +9,13 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/register">Register</router-link> |
-      <button>Logout</button>
+      <button @click="userStore.logoutUser">Logout</button>
     </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<script setup>
+import { useUserStore } from "./stores/user";
+const userStore = useUserStore()
+</script>
--- firebase-app/src/firebaseConfig.js
+++ firebase-app/src/firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQQbnq6UluAC7ODwpfX36Bun2StrNYuxk",
